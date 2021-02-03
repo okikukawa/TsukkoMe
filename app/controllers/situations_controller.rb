0c2c951b791d14d698136d044a6d9aa5e113c2f1
okikukawa <kikukawa0131@gmail.com>
2021-02-03 16:57:32 +0900

    [add]situationのCRUD機能を実装

--- app/controllers/situations_controller.rb
+++ app/controllers/situations_controller.rb
@@ -1,4 +1,32 @@
 class SituationsController < ApplicationController
+  before_action :set_situation, only:[:show, :destroy]
   def index
+    @situation = Situation.all
+  end
+
+  def new
+    @situation = Situation.new
+  end
+
+  def create
+    Situation.create(situation_params)
+    redirect_to situations_path, notice: "シチュエーションを作成しました。"
+  end
+
+  def show
+  end
+
+  def destroy
+    @situation.destroy
+    redirect_to situations_path, notice: "シチュエーションを削除しました。"
+  end
+
+  private
+  def situation_params
+    params.require(:situation).permit(:title)
+  end
+
+  def set_situation
+    @situation = Situation.find(params[:id])
   end
 end
