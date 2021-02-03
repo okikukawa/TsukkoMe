e97d4dfa9b8fe9371afa1b0eaaace369861f1c4c
okikukawa <kikukawa0131@gmail.com>
2021-02-03 15:57:29 +0900

    [add]system specを作成

---
+++ spec/system/situation_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+RSpec.describe 'シチュエーション機能' ,type: :system do
+  describe '一覧表示機能' do
+    context '一覧画面に遷移した場合' do
+      it '作成済みのシチュエーションが表示される' do
+      end
+    end
+  end
+end
