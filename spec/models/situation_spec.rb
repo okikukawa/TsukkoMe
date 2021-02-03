7652c340a401b68251567b6003c20234e0863709
okikukawa <kikukawa0131@gmail.com>
2021-02-03 17:54:21 +0900

    [update]model_specのテストを更新

--- spec/models/situation_spec.rb
+++ spec/models/situation_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Situation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '新規作成機能' do
+    context 'titleを空で登録した場合' do
+      it 'バリデーションに引っかかる' do
+        situation = Situation.create(title: "")
+        expect(situation).not_to be_valid
+      end
+    end
+  end
 end
