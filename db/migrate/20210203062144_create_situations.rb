80d3fab809df81e8c801f0358b76f868f8f4f2d1
okikukawa <kikukawa0131@gmail.com>
2021-02-03 15:57:51 +0900

    [update]titleカラムにnotnull制約を追加

--- db/migrate/20210203062144_create_situations.rb
+++ db/migrate/20210203062144_create_situations.rb
@@ -1,7 +1,7 @@
 class CreateSituations < ActiveRecord::Migration[5.2]
   def change
     create_table :situations do |t|
-      t.string :title
+      t.string :title, null: false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 2021_02_03_062144) do
   enable_extension "plpgsql"
 
   create_table "situations", force: :cascade do |t|
-    t.string "title"
+    t.string "title", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
