092c2b3bddd20c46ccfa3d7e5e5fdc9ad8040bc1
okikukawa <kikukawa0131@gmail.com>
2021-02-04 17:43:24 +0900

    [add]ルーティング、コントローラ、ビューを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'tsukkomis/index'
+  get 'tsukkomis/new'
+  get 'tsukkomis/create'
+  get 'tsukkomis/show'
+  get 'tsukkomis/edit'
+  get 'tsukkomis/destroy'
   resources :situations, only: [:index, :show, :new, :create, :destroy]
+  resources :tsukkomis
 end
---
+++ spec/views/tsukkomis/edit.html.erb_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe "tsukkomis/edit.html.erb", type: :view do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
