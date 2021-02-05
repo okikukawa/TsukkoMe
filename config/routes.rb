e41b68285d31feb3b06ba2206998d19ba7578951
okikukawa <kikukawa0131@gmail.com>
2021-02-06 01:36:35 +0900

    [update]deviseを日本語化

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users
+  resources :users, only:[:show]
   resources :situations, only: [:index, :show, :new, :create, :destroy] do
     resources :tsukkomis
   end
