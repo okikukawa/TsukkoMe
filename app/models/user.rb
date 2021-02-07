8f8e110a9e0240325e8485d86c5de05c75e7fb50
okikukawa <kikukawa0131@gmail.com>
2021-02-07 17:54:37 +0900

    [update]userテーブルのnameにバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   # :recoverable
+  validates :name, presence: true, length: { maximum:30 }
   devise :database_authenticatable, :registerable,
          :rememberable, :validatable
   has_many :situations
