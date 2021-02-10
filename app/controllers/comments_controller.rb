096468a0bbaf902c0a924c8fa5d6eaf64263aab3
okikukawa <kikukawa0131@gmail.com>
2021-02-10 21:27:30 +0900

    [update]コメントの新規投稿をajaxで実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,11 +5,20 @@ class CommentsController < ApplicationController
     @comment.user_id = current_user.id
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to situation_tsukkomi_path(@comment.tsukkomi.situation_id, @comment.tsukkomi.id) }
+        # format.html { redirect_to situation_tsukkomi_path(@comment.tsukkomi.situation_id, @comment.tsukkomi.id) }
+        format.js { render :index }
       else
         format.html { redirect_to situation_tsukkomi_path(@comment.tsukkomi.situation_id, @comment.tsukkomi.id), notice: "コメントの投稿に失敗しました。" }
       end
     end
+  end
+
+  def edit
+
+  end
+
+  def destroy
+
   end
 
   private
