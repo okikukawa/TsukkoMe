76320d8838ed9a31e29847a33532dd432f15d149
okikukawa <kikukawa0131@gmail.com>
2021-02-10 16:52:28 +0900

    [add]ランダムシチュエーション表示機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :users, only:[:show]
   resources :situations, only:[:index, :show, :new, :create, :destroy] do
     resources :tsukkomis
+    get 'thousand_fungo', on: :collection
   end
   resources :favorites, only:[:index, :create, :destroy]
   root to: "situations#index"
