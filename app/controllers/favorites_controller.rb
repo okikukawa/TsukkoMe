4fdeb84ae5d25035f47923029b64ffdef6085bb5
okikukawa <kikukawa0131@gmail.com>
2021-02-10 10:41:56 +0900

    [update]お気に入り処理後のredirect先を修正

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -5,11 +5,11 @@ class FavoritesController < ApplicationController
 
   def create
     favorite = current_user.favorites.create(tsukkomi_id: params[:tsukkomi_id])
-    redirect_to situations_path, notice: "#{favorite.tsukkomi.user.name}さんのツッコミをお気に入り登録しました。"
+    redirect_to situation_tsukkomi_path(favorite.tsukkomi.situation_id, favorite.tsukkomi.id), notice: "#{favorite.tsukkomi.user.name}さんのツッコミをお気に入り登録しました。"
   end
 
   def destroy
     favorite = current_user.favorites.find_by(id: params[:id]).destroy
-    redirect_to situations_path, notice: "#{favorite.tsukkomi.user.name}さんのツッコミをお気に入り解除しました。"
+    redirect_to situation_tsukkomi_path(favorite.tsukkomi.situation_id, favorite.tsukkomi.id), notice: "#{favorite.tsukkomi.user.name}さんのツッコミをお気に入り解除しました。"
   end
 end
