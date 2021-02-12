8042e7e06ef3ca05c2f23035f3b8de09ec932d52
okikukawa <kikukawa0131@gmail.com>
2021-02-12 19:31:58 +0900

    [update]検索機能のテストを実装(system_spec)

--- spec/factories/tsukkomis.rb
+++ spec/factories/tsukkomis.rb
@@ -2,4 +2,7 @@ FactoryBot.define do
   factory :tsukkomi do
     phrase { 'ツッコミ1' }
   end
+  factory :tsukkomi2, class: User do
+    phrase { 'ツッコミ2' }
+  end
 end
--- spec/system/tsukkomi_spec.rb
+++ spec/system/tsukkomi_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe 'ツッコミ機能' ,type: :system do
     user = FactoryBot.create(:user)
     @situation = FactoryBot.create(:situation, user: user)
     @tsukkomi = FactoryBot.create(:tsukkomi, situation: @situation, user: user)
+    # @tsukkomi2 = FactoryBot.create(:tsukkomi2, situation: @situation, user: user)
   end
   describe '新規作成機能' do
     context '新しくツッコミを作成した場合' do
@@ -51,6 +52,32 @@ RSpec.describe 'ツッコミ機能' ,type: :system do
         page.accept_confirm do
           click_link '削除', href: situation_tsukkomi_path(@situation, @tsukkomi)
         end
+        expect(page).not_to have_content 'ツッコミ1'
+      end
+    end
+  end
+  describe 'ツッコミ検索機能' do
+    before do
+      visit new_user_session_path
+      fill_in 'Eメール', with: 'user1@test.com'
+      fill_in 'パスワード', with: 'password'
+      click_button 'ログイン'
+    end
+    context '部分一致で検索した場合' do
+      it '検索結果に一致したツッコミが表示される' do
+        visit situation_path(@situation)
+        fill_in 'q_phrase_cont', with: 'ツッコミ'
+        click_button '検索'
+        expect(page).to have_content '"ツッコミ"の検索結果'
+        expect(page).to have_content 'ツッコミ1'
+      end
+    end
+    context '検索結果が一致しない場合' do
+      it 'ツッコミが表示されない' do
+        visit situation_path(@situation)
+        fill_in 'q_phrase_cont', with: 'ツッコミ3'
+        click_button '検索'
+        expect(page).to have_content '"ツッコミ3"の検索結果'
         expect(page).not_to have_content 'ツッコミ1'
       end
     end
