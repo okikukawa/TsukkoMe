13841c9d0a6fef13a4bd9c4f14eb835262c9b530
okikukawa <kikukawa0131@gmail.com>
2021-02-16 19:11:49 +0900

    [update]お気に入り機能のテストをsystem_specで実装

--- spec/system/tsukkomi_spec.rb
+++ spec/system/tsukkomi_spec.rb
@@ -2,9 +2,10 @@ require 'rails_helper'
 RSpec.describe 'ツッコミ機能' ,type: :system do
   before do
     user = FactoryBot.create(:user)
-    @situation = FactoryBot.create(:situation, user: user)
+    user2 = FactoryBot.create(:user2)
+    @situation = FactoryBot.create(:situation, user: user2)
     @tsukkomi = FactoryBot.create(:tsukkomi, situation: @situation, user: user)
-    # @tsukkomi2 = FactoryBot.create(:tsukkomi2, situation: @situation, user: user)
+    # @tsukkomi2 = FactoryBot.create(:tsukkomi2, situation: @situation, user: user2)
   end
   describe '新規作成機能' do
     context '新しくツッコミを作成した場合' do
@@ -13,10 +14,9 @@ RSpec.describe 'ツッコミ機能' ,type: :system do
         fill_in 'Eメール', with: 'user1@test.com'
         fill_in 'パスワード', with: 'password'
         click_button 'ログイン'
-        visit situation_path(@situation)
-        click_link 'ツッコミを作成する'
-        fill_in 'フレーズ', with: 'new_tsukkomi1'
-        click_button '登録する'
+        visit new_situation_tsukkomi_path(@situation)
+        fill_in 'tsukkomi_phrase', with: 'new_tsukkomi1'
+        click_button '投稿する！'
         expect(page).to have_content 'new_tsukkomi1'
       end
     end
@@ -30,45 +30,52 @@ RSpec.describe 'ツッコミ機能' ,type: :system do
     end
     context '詳細画面に遷移した場合' do
       it 'ツッコミの詳細を確認できる' do
-        visit situation_path(@situation)
-        click_link 'ツッコミ1'
+        visit situation_tsukkomi_path(@situation, @tsukkomi)
         expect(page).to have_content 'ツッコミ1'
       end
     end
     context 'ツッコミを編集した場合' do
       it '編集後のツッコミが表示される' do
-        visit situation_path(@situation)
-        click_link 'ツッコミ1'
+        visit situation_tsukkomi_path(@situation, @tsukkomi)
+        click_on 'dropdownMenu1'
         click_link '編集'
-        fill_in 'フレーズ', with: 'ツッコミ1！'
-        click_button '更新する'
+        fill_in 'tsukkomi_phrase', with: 'ツッコミ1！'
+        click_button '更新'
         expect(page).to have_content 'ツッコミ1！'
       end
     end
     context 'ツッコミを削除した場合' do
       it 'ツッコミ詳細に表示されない' do
-        visit situation_path(@situation)
-        click_link 'ツッコミ1'
+        visit situation_tsukkomi_path(@situation, @tsukkomi)
+        click_on 'dropdownMenu1'
         page.accept_confirm do
           click_link '削除', href: situation_tsukkomi_path(@situation, @tsukkomi)
         end
         expect(page).not_to have_content 'ツッコミ1'
       end
     end
+    context 'ツッコミをお気に入りした場合' do
+      it 'フラッシュメッセージが表示される' do
+        visit situation_tsukkomi_path(@situation, @tsukkomi)
+        click_link 'favorite-icon'
+        expect(page).to have_content 'ユーザー1さんのツッコミをお気に入り登録しました。'
+      end
+    end
+    context 'ツッコミをお気に入り解除した場合' do
+      it 'フラッシュメッセージが表示される' do
+        visit situation_tsukkomi_path(@situation, @tsukkomi)
+        click_link 'favorite-icon'
+        click_link 'favorite-icon'
+        expect(page).to have_content 'ユーザー1さんのツッコミをお気に入り解除しました。'
+      end
+    end
   end
   describe 'ツッコミ検索機能' do
-    before do
-      visit new_user_session_path
-      fill_in 'Eメール', with: 'user1@test.com'
-      fill_in 'パスワード', with: 'password'
-      click_button 'ログイン'
-    end
     context '部分一致で検索した場合' do
       it '検索結果に一致したツッコミが表示される' do
         visit situation_path(@situation)
         fill_in 'q_phrase_cont', with: 'ツッコミ'
         click_button '検索'
-        expect(page).to have_content '"ツッコミ"の検索結果'
         expect(page).to have_content 'ツッコミ1'
       end
     end
@@ -77,7 +84,6 @@ RSpec.describe 'ツッコミ機能' ,type: :system do
         visit situation_path(@situation)
         fill_in 'q_phrase_cont', with: 'ツッコミ3'
         click_button '検索'
-        expect(page).to have_content '"ツッコミ3"の検索結果'
         expect(page).not_to have_content 'ツッコミ1'
       end
     end
