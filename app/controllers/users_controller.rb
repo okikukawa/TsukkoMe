5e522aec0d52dbc37b8b47f44af50b4a0282a861
okikukawa <kikukawa0131@gmail.com>
2021-02-17 12:21:28 +0900

    [add]マイページ内で投稿したツッコミとお気に入り登録したツッコミを切り替えられる機能を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,10 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @tsukkomis = @user.tsukkomis.sort_created_at.page(params[:page]).per(10)
+  end
+
+  def favorite
+    @user = User.find(params[:id])
     @favorites = @user.favorites.sort_created_at.page(params[:page]).per(10)
   end
 end
