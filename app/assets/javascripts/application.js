41352ec23b4cbad64a38c1ce03d4e28370113f3e
okikukawa <kikukawa0131@gmail.com>
2021-02-27 23:21:00 +0900

    [update]bootstrap関連の設定を修正

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,5 +14,6 @@
 //= require activestorage
 //= require turbolinks
 //= require jquery/dist/jquery.js
+//= require popper.js/dist/popper.js
 //= require bootstrap/dist/js/bootstrap.min
 //= require_tree .
