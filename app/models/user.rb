8cd1461c657df899c99bbddd1b9e11c9539e4b7e
okikukawa <kikukawa0131@gmail.com>
2021-02-27 16:06:05 +0900

    [update]ゲストログイン機能を実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,9 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :comments, dependent: :destroy
   mount_uploader :image, ImageUploader
+  def self.guest
+    find_or_create_by!(name: 'ゲスト', email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  root to: "situations#index"
   devise_for :users
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#new_guest'
+  end
   resources :users, only:[:show] do
     get 'favorite', on: :member
   end
@@ -11,5 +15,5 @@ Rails.application.routes.draw do
     get 'search', on: :member
   end
   resources :favorites, only:[:index, :create, :destroy]
-  root to: "situations#index"
+
 end
