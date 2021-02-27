32fe428e21514e1596656630b1a167ee710fea22
okikukawa <kikukawa0131@gmail.com>
2021-02-27 16:23:27 +0900

    [update]ゲストユーザーでプロフィールを変更できない設定

---
+++ app/controllers/users/registrations_controller.rb
@@ -0,0 +1,9 @@
+class Users::RegistrationsController < Devise::RegistrationsController
+  before_action :check_guest, only:[:edit, :update]
+
+  def check_guest
+    if current_user.email = "guest@example.com"
+      redirect_to user_path(current_user), alert: "ゲストユーザーでマイページの編集を行うことはできません。"
+    end
+  end
+end
