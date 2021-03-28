54f77402a525b73293c4c72ec010b013b07e827f
okikukawa <kikukawa0131@gmail.com>
2021-03-28 21:42:13 +0900

    [update]awsデプロイのためuploaderを修正

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -5,7 +5,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   # storage :file
-  storage :fog
+  # storage :fog
   process :resize_to_limit => [200, 200]
 
   # Override the directory where uploaded files will be stored.
