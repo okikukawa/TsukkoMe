97cfce3aa0d21ceff1ca5ac52b8407a502c4c234
okikukawa <kikukawa0131@gmail.com>
2021-04-14 22:54:44 +0900

    [update]ファイル名を修正

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -4,7 +4,6 @@ class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
 
   # Choose what kind of storage to use for this uploader:
-  # storage :file
   storage :fog
   process :resize_to_limit => [200, 200]
 
